405f7ffe625344588ab7c8518eaee03342e22617
dagarwal2 <dagarwal2@bajajauto.co.in>
2025-11-17 19:16:12 +0530

    created env variable for backend url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://10.118.206.245:8000/",
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: import.meta.env.BACKEND_URL,
   // baseURL: "http://10.11.26.107:8000/",
   // baseURL: "http://127.0.0.1:8000/",
 });
