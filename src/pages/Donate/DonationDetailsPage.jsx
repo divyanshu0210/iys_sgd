da7475b096f4e68e33e69c48b693eb2631dbe577
dagarwal2 <dagarwal2@bajajauto.co.in>
2026-01-23 10:53:14 +0530

    donation detail page header changed

--- src/pages/Donate/DonationDetailsPage.jsx
+++ src/pages/Donate/DonationDetailsPage.jsx
@@ -5,7 +5,7 @@ export default function DonationDetailsPage() {
   return (
     <>
      <h1 className="donate-heading" style={{ textAlign: "center" ,margin: "10px"}}>Donation</h1>
-     <p style={{ textAlign: "center" ,margin: "20px"}}>Please fill all fields & Submit below to make your donation.</p>
+     <p style={{ textAlign: "center" ,margin: "10px" , fontSize:"15px"}}>Please fill all fields & Submit below to make your donation.</p>
     <DonationForm />
     </>
   );
